6726ed222c39430307a95d3e1328608cd43a0a7e
Kirk <Robertson>
2017-11-24 12:20:34 +0200

    - remove udm build

--- es/utils/isStatePlainEnough.js
+++ es/utils/isStatePlainEnough.js
@@ -1,6 +1,6 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-import isPlainObject from 'lodash/isPlainObject';
+import isPlainObject from 'lodash-es/isPlainObject';
 
 export default function isStatePlainEnough(a) {
   // isPlainObject + duck type not immutable
